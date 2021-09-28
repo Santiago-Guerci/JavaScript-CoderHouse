9e8bb767cd3e7c6917642d77f41cbc701d451e46
Santiago-Guerci <santiago.38@hotmail.com>
2021-09-27 23:56:17 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var numeroA = 1;
+let numeroB = 2;
+const NUMEROB = 3;
+
+let resultadoSuma = numeroA + numeroB
+console.log(resultadoSuma);
+
+var texto1 = "CODER";
+let texto2 = "HOUSE";
+const BLANCO = " ";
+
+let concatenado = texto1 + texto2
+console.log(concatenado);
+
+let numero1 = parseInt(prompt("Ingrese un número: "));
+let numero2 = parseInt(prompt("Ingrese otro número: "));
+
+let resultado = numero1 + numero2;
+
+alert("El resultado es: " + resultado);
