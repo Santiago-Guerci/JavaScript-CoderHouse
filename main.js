06617152294201bb638d30c3bab478b1004e84eb
Santiago Güerci <santiago.38@hotmail.com>
2021-10-13 20:17:08 -0300

    simulador clase 4

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
-//Imprimir una rampa de x filas según lo ingresado.
+// Ingreso de monto en dólares
 
-//Mientras no sea un número entre el 1 y el 10, voy a seguir pidiendo que lo ingrese.
-let num;
+let dolares = parseInt(prompt("Ingrese el monto de su compra en dólares: "));
+let cambio = parseInt(prompt("¿A qué valor le realizan el cambio?"));
+let valor;
+let impuestoPais;
+let impuestoGan;
+let final;
+let total;
 
-do {
-    num = parseInt(prompt("Ingrese un número del 1 al 10: "));
-} while (num<1 || num>10);
+let ganancias = confirm("¿Usted paga Impuesto a las Ganancias o Bienes Personales?");
+let moneda = prompt("¿Pagará en dólares o en pesos?");
 
-for (let i = 0; i < num; i++) 
-{
-    console.log("#".repeat(i+1));
+function calcularImpuestos (monto) {
+    valor = dolares * cambio;
+    impuestoPais = valor * 0.30;
+    impuestoGan = valor * 0.35;
+    total = valor + impuestoGan + impuestoPais;
 }
 
-//Encontre la forma de escribir múltiples caracteres uno al lado del otro en consola usando el .repeat()
-//Ya que sino me aparecía el caracter "#" y el num de la cantidad de veces que había sido impreso.
+function metodoPago (metodo) {
+
+    if (metodo == "dolares") {
+        final = valor + impuestoGan;
+    } else if (metodo == "pesos") {
+        final = valor + impuestoGan + impuestoPais;
+    } else {
+        alert("Debe ingresar 'dolares' o 'pesos'");
+    }
+}
+
+calcularImpuestos (dolares);
+metodoPago (moneda);
+
+if (ganancias) {
+    alert("Su monto en dolares es: USD" + dolares + 
+    "\nEl valor al cambio ingresado: $" + valor +
+    "\nImpuesto País: $" + impuestoPais + 
+    "\nImpuesto a las Ganancias: $" + impuestoGan + 
+    "\nTotal: $" + total +
+    "\nAl abonar en "+ moneda + ": $" + final + 
+    "\nUsted puede devolver el 35% de Ganancias ingresando en AFIP");
+    
+} else {
+    alert("Su monto en dolares es: USD" + dolares + 
+    "\nEl valor al cambio ingresado: $" + valor +
+    "\nImpuesto País: $" + impuestoPais + 
+    "\nImpuesto a las Ganancias: $" + impuestoGan + 
+    "\nTotal: $" + total +
+    "\nAl abonar en "+ moneda + ": $" + final + 
+    "\nUsted NO puede devolver el 35% de Ganancias ingresando en AFIP");
+}
