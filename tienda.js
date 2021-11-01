755fd2bc86ac2b0e97dcc71e5e24bf4bbd388d52
Santiago Güerci <santiago.38@hotmail.com>
2021-11-01 20:20:49 -0300

    Intento fallido

--- tienda.js
+++ tienda.js
@@ -1,2 +1,11 @@
-let boton = document.getElementById("button");
-boton.addEventListener('click', agregarElementoLista());
+let boton = document.getElementById('button');
+let productos = document.getElementById('listaProductos');
+let elementoLista = document.createElement('li');
+elementoLista.innerHTML = "Producto agregado";
+
+boton.onclick = () => {elementoLista.classList.add("list-group-item"); productos.appendChild(elementoLista);}
+
+// function agregarElementoLista() {
+//     elementoLista.classList.add("list-group-item");
+//     productos.appendChild(elementoLista);
+// }
